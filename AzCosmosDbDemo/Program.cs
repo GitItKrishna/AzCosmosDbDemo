a7244bb84485f77e7fc12f0e2892121ba78a83ed
krishna kumar <krishna.kristamshetty@dhcs.ca.gov>
2025-01-05 19:05:12 -0600

    Initial Commit for Az Cosmos Db

---
+++ AzCosmosDbDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Azure.Cosmos;
+
+string connectionString="AccountEndpoint=https://myazcosmosdbinstance.documents.azure.com:443/;AccountKey=e2qYKqY9NrS95G5YSgRAJGkh79VQN1DH3QGQ6OmpFfezF5dW9TldvvJgj90Z9oKAqEtXJaC3EXY7ACDbe417Dw==;";
+CosmosClient client = new CosmosClient(connectionString);
+Database database = await client.CreateDatabaseIfNotExistsAsync("myappdb59");
+Console.WriteLine("Database created with id: {0}", database.Id);
+
+string partitionKey = "/username";
+Container container = await database.CreateContainerIfNotExistsAsync("users", partitionKey);
+Console.WriteLine("Container created with id: {0}", container.Id);
